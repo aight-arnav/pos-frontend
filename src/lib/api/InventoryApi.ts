daf822dab077778855a7737b984bfd9c76af25b9
Arnav Anand <arnav.anand@increff.com>
2026-02-10 12:21:14 +0530

    handling error tsv download

--- src/lib/api/InventoryApi.ts
+++ src/lib/api/InventoryApi.ts
@@ -1,5 +1,7 @@
 import { InventoryData, InventoryForm } from "@/lib/types/Inventory";
 import { ApiClient, MultipartApiClient } from "./ApiClient";
+import { AxiosError } from "axios";
+import toast from "react-hot-toast";
 
 export const InventoryApi = {
   getAll: async (page: number = 0, size: number = 10) => {
@@ -22,10 +24,40 @@ export const InventoryApi = {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await MultipartApiClient.post<InventoryData[]>(
-      "/inventory/bulk",
-      formData
-    );
-    return res.data;
+    try {
+      const res = await MultipartApiClient.post(
+        "/inventory/bulk",
+        formData,
+        { responseType: "json" }
+      );
+      return res.data;
+    } catch (error: unknown) {
+      let response;
+      if (error instanceof AxiosError) {
+        response = error.response;
+      }
+
+      if (
+        response?.headers["content-type"]?.includes("text/tab-separated-values")
+      ) {
+        const blob = new Blob([response.data], {
+          type: "text/tab-separated-values",
+        });
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "inventory-errors.tsv";
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+
+        toast.error("Some rows failed. Error file downloaded.");
+        return;
+      }
+
+      throw error;
+    }
   },
 };
